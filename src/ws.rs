249f885f5398af78c0ae814eb5250d8db2523849
Kevin Matthews <kevin@junelife.com>
2024-01-19 12:12:09 +1100

    Bump ESP-IDF requirement to major version 5

--- src/ws.rs
+++ src/ws.rs
@@ -7,7 +7,7 @@ pub use embedded_svc::ws::{Final, Fragmented, FrameType};
     esp_idf_comp_tcp_transport_enabled,
     esp_idf_comp_esp_tls_enabled,
     any(
-        all(esp_idf_version_major = "4", esp_idf_ws_transport),
+        all(esp_idf_version_major = "5", esp_idf_ws_transport),
         esp_idf_comp_espressif__esp_websocket_client_enabled
     )
 ))]
